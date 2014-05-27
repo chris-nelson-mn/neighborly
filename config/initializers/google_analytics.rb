7aae03b991630429915775ed4b9e54e8a0122afc
Chris Nelson <chris.nelson.mn@gmail.com>
2014-05-26 23:58:31 -0500

    Fix Configurations table check in some initializers

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,2 +1,2 @@
 # Google analytics ID
-GA.tracker = Configuration['google_analytics_id'] if Rails.env.production? && Configuration['google_analytics_id'].present? && ActiveRecord::Base.connection.tables.include?(::Configuration.table_name)
+GA.tracker = Configuration['google_analytics_id'] if Rails.env.production? && ActiveRecord::Base.connection.tables.include?(::Configuration.table_name) && Configuration['google_analytics_id'].present?
